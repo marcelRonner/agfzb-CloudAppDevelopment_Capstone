03ce6668df5ce65840fcbb8665b0d7ff025ebe08
marcelronner <marcel.ronner@gmail.com>
2023-01-25 15:25:31 -0500

    Integrated car makes / car models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+from .models import CarMake, CarModel
 # from .models import related models
 
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 6
 
-# Register your models here.
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name',  'dealer_id', 'car_type', 'year']
+    search_fields = ['name']
 
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name', 'description']
+    search_fields = ['name']
 
 # Register models here
+
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake)
